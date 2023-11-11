96436f4f428df82367046247b82c2205b15ef41c
KJWHUB <art.1025p@gmail.com>
2023-11-11 21:58:11 +0900

    Time: 56 ms (40.89%), Space: 42 MB (86.61%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+const reduce = (nums, fn, init) => {
+    if (nums.length === 0) return init
+    
+    let accum = init
+    
+    for(let i = 0; i< nums.length; i++) {
+        accum = fn(accum, nums[i])
+    }
+    
+    return accum
+};
