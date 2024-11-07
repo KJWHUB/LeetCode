abb7f3b7ac81a7f847011ac1ec1d4f9b028549d1
Kwon Jung Woon <art.1025p@gmail.com>
2024-11-07 23:51:45 +0900

    Time: 0 ms (100%), Space: 49.4 MB (70.01%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+const addBinary = function(a, b) {
+    const aBin = `0b${a}`
+    const bBin = `0b${b}`
+    const sum = BigInt(aBin) + BigInt(bBin)
+    return sum.toString(2)
+};
