5874a1d32800a66c241f4aa78cfef1778190bff6
Kwon Jung Woon <art.1025p@gmail.com>
2024-11-17 21:35:52 +0900

    Time: 1 ms (89.15%), Space: 51.2 MB (32.36%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum-better.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+    const map = new Map()
+
+    for (let i = 0; i < nums.length; i++){
+        const n = nums[i]
+        const complement = target - n;
+
+        if (map.has(complement)) {
+            return [map.get(complement), i]
+        }
+
+        map.set(n, i)
+    }
+
+    return[]
+};
