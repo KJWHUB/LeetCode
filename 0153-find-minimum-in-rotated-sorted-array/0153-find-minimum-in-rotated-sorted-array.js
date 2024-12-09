07b97ecf99bd983e8f9d15e5606a057df940127c
Kwon Jung Woon <art.1025p@gmail.com>
2024-12-09 22:36:05 +0900

    Time: 0 ms (100%), Space: 48.9 MB (64.54%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMin = function(nums) {
+
+    // o(n)
+    // return Math.min(...nums)
+
+    let left = 0
+    let right = nums.length - 1
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2)
+
+        if (nums[mid] < nums[right]) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    
+    return nums[left];
+};
