0bb30193cdf01d1a38ab09bd75c61eba4efd1459
Kwon Jung Woon <art.1025p@gmail.com>
2024-12-27 06:15:28 +0900

    Time: 8 ms (65.58%), Space: 54.6 MB (63.27%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+const buildTree = function(preorder, inorder) {
+    // hash setup
+    const hash = {}
+    for (let i = 0; i < inorder.length; i++) {
+        hash[inorder[i]] = i
+    }
+
+    const rec = (start, end) => {
+        if (start > end) return null
+
+        const val = preorder.shift()
+        const root = new TreeNode(val)
+        root.left = rec(start, hash[val] - 1)
+        root.right = rec(hash[val] + 1, end)
+
+        return root
+    }
+
+    return rec(0, preorder.length - 1)
+};
